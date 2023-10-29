83b16164c00e0f885e6f47694753c8716cd9bfae
dani001024 <aj9473@naver.com>
2023-10-29 16:24:15 +0900

    button컴포넌트 수정

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,56 +1,30 @@
+import React, { CSSProperties } from 'react';
 import styled from 'styled-components';
-// import ".././Fonts/font.css";
 
-interface ButtonProps {
+interface CustomButton {
   children: string;
   onClick?: () => void;
-  page: number;
+  backgroundColor: CSSProperties['color'];
+  color: CSSProperties['color'];
+  width: CSSProperties['width'];
 }
 
-const Button = ({ children, onClick, page }: ButtonProps) => {
-  // page 값에 따라 다른 onClick 함수 설정
-  const handleClick = () => {
-    if (page === 1) {
-      // page가 1일 때의 처리
-    } else if (page === 2) {
-      // page가 2일 때의 처리
-    } else if (page === 3) {
-      // page가 2일 때의 처리
-    } else if (page === 4) {
-      // page가 2일 때의 처리
-    } else if (page === 5) {
-      // page가 2일 때의 처리
-    } else {
-      // 그 외의 경우
-    }
-  };
+interface ButtonProps {
+  button: CustomButton;
+}
+
+interface styledButtonProps {
+  boxColor: CSSProperties['color'];
+  color: CSSProperties['color'];
+}
+
+const Button = (props: ButtonProps) => {
+  const { button } = props;
+  const { children, backgroundColor, width, color } = button;
+
   return (
-    <ParentContainer
-      style={{
-        width: (() => {
-          if (children === 'START') return '327px';
-          if (page === 6 && children === '이미지 저장') return '155px';
-          if (page === 6) return '70px';
-          return '156px';
-        })(),
-      }}
-    >
-      <StyledButton
-        onClick={handleClick}
-        style={{
-          backgroundColor: (() => {
-            if (children === '이미지 저장') return '#333';
-            if (children === '이전' || children === '초기화') return '#ffff';
-            if (children === '다음' && page === 6) return '#ffff';
-            return '#7F4C2F'; // 기본 배경색
-          })(),
-          color: (() => {
-            if (children === '이전' || children === '초기화') return 'black';
-            if (children === '다음' && page === 6) return 'black';
-            return 'white';
-          })(),
-        }}
-      >
+    <ParentContainer width={width}>
+      <StyledButton boxColor={backgroundColor} color={color}>
         {children}
       </StyledButton>
     </ParentContainer>
@@ -59,7 +33,7 @@ const Button = ({ children, onClick, page }: ButtonProps) => {
 
 export default Button;
 
-const StyledButton = styled.button`
+const StyledButton = styled.button<styledButtonProps>`
   padding: 10px 10px;
   border: 2px solid black;
   border-radius: 5px;
@@ -67,16 +41,19 @@ const StyledButton = styled.button`
   font-family: Galmuri11;
   width: 100%;
   height: 100%;
-  font-size: 16px;
+  font-size: 16px
+  background-color: ${(props) => props.boxColor};
+  color: ${(props) => props.color};
+
 `;
 
-const ParentContainer = styled.div`
+const ParentContainer = styled.div<{ width: CSSProperties['width'] }>`
   box-shadow: 5px 5px 0px #ffbb00;
-  width: 100%;
+  width: ${(props) => props.width};
   height: 100%;
   border-radius: 5px;
   &:active {
-    box-shadow: none; /* 클릭 시 그림자 없앰 */
+    box-shadow: none;
     transform: translate(6px, 6px);
   }
 `;
